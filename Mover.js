3dadc8be74594ac50b5238acd40ebcfaadf8b4f4
2123025 <lsi371@naver.com>
2021-11-02 16:25:10 +0900

    Use array, make attract and repel
    배열 사용, mover.attract()와 mover.repel()을 만들어 각각 인력과 척력을 표현

--- Mover.js
+++ Mover.js
@@ -4,22 +4,36 @@ class Mover {
     this.vel = createVector();
     this.acc = createVector();
     this.m = m;
-    
+
   }
-  
+
   applyForce(aForce) {
     let f = p5.Vector.div(aForce, this.m);
     this.acc.add(f);
   }
-  
+
   update() {
     this.vel.add(this.acc);
     this.pos.add(this.vel);
     this.acc.set(0, 0);
   }
-  
+
   show() {
     fill(0);
     circle(this.pos.x, this.pos.y, this.m);
+  }
+
+  attract(aPos) {
+    let f = p5.Vector.sub(aPos, this.pos);
+
+    f.limit(2);
+    this.applyForce(f);
+  }
+
+  repel(aPos) {
+    let f = p5.Vector.sub(this.pos, aPos);
+
+    f.limit(2);
+    this.applyForce(f);
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,16 +1,33 @@
-let m;
+let movers = [];
+let canvas;
 
 function setup() {
   createCanvas(400, 400);
-  m = new Mover(50, 50, 10);
+  canvas = createGraphics(width, height);
+
+  for(let i = 0; i < 5; i++) {
+    let mover = new Mover(random(400), random(400), random(5, 50));
+    movers.push(mover);
+  }
 }
 
 function draw() {
-  background(220);
-  m.update();
-  m.show();
-  
-  if (mouseIsPressed) {
-    //make attract to mouse position
+  background(240);
+  image(canvas, 0, 0);
+
+  for(let i = 0; i < movers.length; i++) {
+    let mover = movers[i];
+    let mousePos = createVector(mouseX, mouseY);
+
+    mover.update();
+    mover.show();
+
+    if(mouseIsPressed) {
+      mover.attract(mousePos);
+    }
+
+    if(keyIsPressed) {
+      mover.repel(mousePos);
+    }
   }
 }
